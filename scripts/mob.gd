04c6b8523f5656303710bce709d4921eb0a43e96
Matixs1330 <xxmatixsxx@gmail.com>
2025-11-02 15:09:08 -0300

    Terminado

--- scripts/mob.gd
+++ scripts/mob.gd
@@ -1,23 +1,33 @@
 extends CharacterBody3D
 signal squashed
 
-@export var min_speed = 10
-@export var max_speed = 18
+@export var min_speed: float = 10.0
+@export var max_speed: float = 18.0
 
-func initialize(start_position, player_position):
+func initialize(start_position: Vector3, player_position: Vector3) -> void:
 	look_at_from_position(start_position, player_position, Vector3.UP)
-	rotate_y(randf_range(-PI / 4, PI / 4))
-	
-	var random_speed = randi_range(min_speed, max_speed)
+	rotate_y(randf_range(-PI / 4.0, PI / 4.0))
+
+	var random_speed: float = randf_range(min_speed, max_speed)
 	velocity = Vector3.FORWARD * random_speed
 	velocity = velocity.rotated(Vector3.UP, rotation.y)
 
-func _physics_process(_delta):
+	
+	if $AnimationPlayer.has_animation("float"):
+		$AnimationPlayer.play("float")
+	else:
+		var anims = $AnimationPlayer.get_animation_list()
+		if anims.size() > 0:
+			$AnimationPlayer.play(anims[0])
+
+	$AnimationPlayer.speed_scale = random_speed / min_speed
+
+func _physics_process(_delta: float) -> void:
 	move_and_slide()
 
 func _on_visible_on_screen_notifier_3d_screen_exited() -> void:
 	queue_free()
-	
-func squash():
+
+func squash() -> void:
 	squashed.emit()
 	queue_free()
--- scripts/player.gd
+++ scripts/player.gd
@@ -24,6 +24,10 @@ func _physics_process(delta):
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
 		$Pivot.basis = Basis.looking_at(direction)
+		$AnimationPlayer.speed_scale = 4
+	else:
+		$AnimationPlayer.speed_scale = 1
+	$Pivot.rotation.x = PI / 6 * velocity.y / jump_impulse
 	
 	target_velocity.x = direction.x * speed
 	target_velocity.z = direction.z * speed
